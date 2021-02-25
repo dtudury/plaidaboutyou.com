299b6acdd0b579ddfac92a216edaeb6081255914
David Tudury <david.tudury@gmail.com>
2021-02-24 23:41:33 -0800

    closer

--- docs/Drawdown.js
+++ docs/Drawdown.js
@@ -1,9 +1,16 @@
+/* eslint-disable accessor-pairs */
 import { createProgram, createShader } from './webglHelpers.js'
 
 export class Drawdown {
   constructor (gl) {
     this.gl = gl
     this.program = _createProgram(gl)
+    gl.useProgram(this.program)
+    this.triangleVertexBufferObject = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.triangleVertexBufferObject)
+    this.positionAttribLocation = gl.getAttribLocation(this.program, 'vertPosition')
+    gl.vertexAttribPointer(this.positionAttribLocation, 4, gl.FLOAT, gl.FALSE, 4 * Float32Array.BYTES_PER_ELEMENT, 0)
+    gl.enableVertexAttribArray(this.positionAttribLocation)
     this.resolutionLocation = gl.getUniformLocation(this.program, 'resolution')
     this.scaleLocation = gl.getUniformLocation(this.program, 'scale')
     this.tieUpLocation = gl.getUniformLocation(this.program, 'tieUp')
@@ -18,6 +25,81 @@ export class Drawdown {
     this.threadingLengthLocation = gl.getUniformLocation(this.program, 'threadingLength')
     this.treadlingLocation = gl.getUniformLocation(this.program, 'treadling')
     this.treadlingLengthLocation = gl.getUniformLocation(this.program, 'treadlingLength')
+  }
+
+  updateVertices () {
+    if (this._scale && this._dimensions) {
+      const step = this._scale
+      const xStep = step
+      const yStep = step
+      this.vertices = []
+      for (let x = 0; x < this._dimensions.width / xStep; ++x) {
+        for (let y = 0; y < this._dimensions.height / yStep; ++y) {
+          this.vertices.push(
+            x, y, 0, 0,
+            x, y, 0, 1,
+            x, y, 1, 0,
+            x, y, 1, 1,
+            x, y, 1, 0,
+            x, y, 0, 1
+          )
+        }
+      }
+      this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.vertices), this.gl.STATIC_DRAW)
+    }
+  }
+
+  draw () {
+    this.gl.drawArrays(this.gl.TRIANGLES, 0, this.vertices.length / 4)
+  }
+
+  setDimensions (dimensions) {
+    this._dimensions = dimensions
+    this.gl.viewport(0, 0, dimensions.width, dimensions.height)
+    this.gl.uniform2f(this.resolutionLocation, dimensions.width, dimensions.height)
+    this.updateVertices()
+  }
+
+  setScale (scale) {
+    this.gl.uniform2f(this.scaleLocation, scale, scale)
+    this._scale = scale
+    this.updateVertices()
+  }
+
+  setColors (colors) {
+    this.gl.uniform3fv(this.colorsLocation, colors)
+  }
+
+  setWarp (warp) {
+    this.gl.uniform1iv(this.warpLocation, warp)
+    this.gl.uniform1f(this.warpLengthLocation, warp.length)
+  }
+
+  setWeft (weft) {
+    this.gl.uniform1iv(this.weftLocation, weft)
+    this.gl.uniform1f(this.weftLengthLocation, weft.length)
+  }
+
+  setThreading (threading) {
+    this.gl.uniform1iv(this.threadingLocation, threading)
+    this.gl.uniform1f(this.threadingLengthLocation, threading.length)
+  }
+
+  setTreadling (treadling) {
+    this.gl.uniform1iv(this.treadlingLocation, treadling)
+    this.gl.uniform1f(this.treadlingLengthLocation, treadling.length)
+  }
+
+  setTieUp (tieUp) {
+    this.gl.uniform1iv(this.tieUpLocation, tieUp)
+  }
+
+  setTreadles (treadles) {
+    this.gl.uniform1f(this.treadlesLocation, treadles)
+  }
+
+  setShafts (shafts) {
+    this.gl.uniform1f(this.shaftsLocation, shafts)
   }
 }
 
--- docs/DrawdownElement.js
+++ docs/DrawdownElement.js
@@ -1,13 +1,44 @@
 import { h, watchFunction } from './horseless.0.5.2.min.esm.js' // '/unpkg/horseless/horseless.js'
-import { createPlaidRenderer } from './plaid.js'
-import { model } from './model.js'
+import { Drawdown } from './Drawdown.js'
+import { model, expandValue } from './model.js'
 
 class DrawdownElement extends window.HTMLCanvasElement {
   connectedCallback () {
-    const drawdownGL = this.getContext('webgl')
-    if (!drawdownGL) { console.error('no webgl?!') }
+    const gl = this.getContext('webgl')
+    if (!gl) { console.error('no webgl?!') }
 
-    const plaidRenderer = createPlaidRenderer(this, drawdownGL)
+    const drawdown = new Drawdown(gl)
+    function plaidRenderer () {
+      const colorValues = []
+      const colorMap = {}
+      const hexMapper = hex => {
+        if (colorMap[hex] == null) {
+          const color = parseInt(hex)
+          const r = ((color >>> 16) & 0xff) / 0xff
+          const g = ((color >>> 8) & 0xff) / 0xff
+          const b = (color & 0xff) / 0xff
+          colorMap[hex] = colorValues.length / 3
+          colorValues.push(r, g, b)
+        }
+        return colorMap[hex]
+      }
+      drawdown.setWarp(expandValue(model.warp, model.vars).map(hexMapper))
+      drawdown.setWeft(expandValue(model.weft, model.vars).map(hexMapper))
+      drawdown.setColors(colorValues.flat())
+
+      const wrap = (i, m) => ((i - 1) % m + m) % m
+      const shaftMapper = i => wrap(i, model.shafts)
+      const treadleMapper = i => wrap(i, model.treadles)
+      drawdown.setThreading(expandValue(model.threading, model.vars).map(shaftMapper))
+      drawdown.setTreadling(expandValue(model.treadling, model.vars).map(treadleMapper))
+
+      drawdown.setDimensions(model.dimensions)
+      drawdown.setScale(model.scale)
+      drawdown.setTieUp(model.tieUp.map(arr => arr.slice().reverse()).flat())
+      drawdown.setTreadles(model.treadles)
+      drawdown.setShafts(model.shafts)
+      drawdown.draw()
+    }
 
     watchFunction(() => {
       this.width = model.dimensions.width
